3878dc63d10bb2206294f2155e9f1ffe50e88fad
Maxime Moreillon <m.moreillon@gmail.com>
2022-04-02 01:32:32 +0000

    meta tags

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 
       <Logo class="logo"/>
 
+      <!-- Multiple H1 is bad practice -->
       <div class="header">
         <h1 class="first_name">Maxime</h1>
         <h1 class="family_name">Moreillon</h1>
@@ -25,7 +26,7 @@
       <div class="links_wrappper">
         <Link
           v-for="(link, index) in links.filter(l => l.category === category)"
-          v-bind:key="`link${index}`"
+          v-bind:key="`link_${index}`"
           :link="link"/>
       </div>
     </section>
@@ -39,7 +40,7 @@
 <script>
 
 import 'vue-material-design-icons/styles.css'
-
+import links from '@/links.js'
 import SubtitleCarousel from '@/components/SubtitleCarousel.vue'
 
 import ScrollIndicator from '@/components/ScrollIndicator.vue'
@@ -61,30 +62,7 @@ export default {
   },
   data(){
     return {
-      links: [
-        {icon: ['fas','newspaper'], label: 'Articles', url: 'https://cms.maximemoreillon.com', category: 'Software development'},
-        {icon: ['fas','tools'], label: 'Projects', url: 'https://articles.maximemoreillon.com/articles?tag_id=111', category: 'Software development'},
-        {icon: ['fab','medium'], label: 'Medium', url: 'https://moreillon.medium.com/', category: 'Software development'},
-        {icon: ['fab','facebook'], label: 'Facebook', url: 'https://www.facebook.com/maxime.moreillon', category: 'Social media'},
-        {icon: ['fab','twitter'], label: 'Twitter', url: 'https://twitter.com/m_moreillon', category: 'Social media'},
-        {icon: ['fab','instagram'], label: 'Instagram', url: 'https://www.instagram.com/maxime_moreillon/', category: 'Social media'},
-        {icon: ['fab','youtube'], label: 'YouTube', url: 'https://www.youtube.com/channel/UCvk0FMjKbin0FGU9LPe-T_g', category: 'Social media'},
-        {icon: ['fab','docker'], label: 'Docker Hub', url: 'https://hub.docker.com/u/moreillon', category: 'Software development'},
-        {icon: ['fab','github'], label: 'GitHub', url: 'https://github.com/maximemoreillon', category: 'Software development'},
-        {icon: ['fab','gitlab'], label: 'GitLab', url: 'https://gitlab.com/moreillon', category: 'Software development'},
-        {icon: ['fab','npm'], label: 'NPM', url: 'https://www.npmjs.com/~moreillon', category: 'Software development'},
-        {icon: ['fas','graduation-cap'], label: 'Google Scholar', url: 'https://scholar.google.com/citations?user=0gkCupAAAAAJ', category: 'Work'},
-        {icon: ['fab','linkedin'], label: 'LinkedIn', url: 'https://www.linkedin.com/in/maxime-moreillon', category: 'Work'},
-        {icon: ['fas','award'], label: 'Patents', url: 'https://patents.google.com/?inventor=Maxime+MOREILLON', category: 'Work'},
-        {icon: ['fas','tools'], label: 'Projects', url: 'https://articles.maximemoreillon.com/articles?tag_id=111', category: 'Mechatronics'},
-        {image: require('@/assets/images/oshwlab.png'), label: 'OSHWLab', url: 'https://oshwlab.com/m.moreillon', category: 'Mechatronics'},
-        {image: require('@/assets/images/artifacthub.png'), label: 'Artifact Hub', url: 'https://artifacthub.io/packages/search?user=moreillon', category: 'Software development'},
-        {image: require('@/assets/images/thingiverse.svg'), label: 'Thingiverse', url: 'https://www.thingiverse.com/moreillon/designs', category: 'Mechatronics'},
-        {icon: ['fab','instagram'], label: 'Instagram', url: 'https://www.instagram.com/maxime_moreillon_modeling', category: 'Modeling'},
-        {image: require('@/assets/images/azs.jpg'), label: 'Agency', url: 'https://azs-group.jp/manage_post/990', category: 'Modeling'},
-        {image: require('@/assets/images/logo.png'), label: 'Profile', url: 'https://modeling.maximemoreillon.com', category: 'Modeling'},
-
-      ]
+      links
     }
 
   },
---
+++ src/links.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    label: 'Articles',
+    icon: ['fas','newspaper'],
+    url: 'https://cms.maximemoreillon.com',
+    category: 'Software development'
+  },
+  {
+    label: 'Projects',
+    icon: ['fas','tools'],
+    url: 'https://articles.maximemoreillon.com/articles?tag_id=111',
+    category: 'Software development'
+  },
+  {
+    label: 'Medium',
+    icon: ['fab','medium'],
+    url: 'https://moreillon.medium.com/',
+    category: 'Software development'
+  },
+  {icon: ['fab','facebook'], label: 'Facebook', url: 'https://www.facebook.com/maxime.moreillon', category: 'Social media'},
+  {icon: ['fab','twitter'], label: 'Twitter', url: 'https://twitter.com/m_moreillon', category: 'Social media'},
+  {icon: ['fab','instagram'], label: 'Instagram', url: 'https://www.instagram.com/maxime_moreillon/', category: 'Social media'},
+  {icon: ['fab','youtube'], label: 'YouTube', url: 'https://www.youtube.com/channel/UCvk0FMjKbin0FGU9LPe-T_g', category: 'Social media'},
+  {icon: ['fab','docker'], label: 'Docker Hub', url: 'https://hub.docker.com/u/moreillon', category: 'Software development'},
+  {icon: ['fab','github'], label: 'GitHub', url: 'https://github.com/maximemoreillon', category: 'Software development'},
+  {icon: ['fab','gitlab'], label: 'GitLab', url: 'https://gitlab.com/moreillon', category: 'Software development'},
+  {icon: ['fab','npm'], label: 'NPM', url: 'https://www.npmjs.com/~moreillon', category: 'Software development'},
+  {icon: ['fas','graduation-cap'], label: 'Google Scholar', url: 'https://scholar.google.com/citations?user=0gkCupAAAAAJ', category: 'Work'},
+  {icon: ['fab','linkedin'], label: 'LinkedIn', url: 'https://www.linkedin.com/in/maxime-moreillon', category: 'Work'},
+  {icon: ['fas','award'], label: 'Patents', url: 'https://patents.google.com/?inventor=Maxime+MOREILLON', category: 'Work'},
+  {icon: ['fas','tools'], label: 'Projects', url: 'https://articles.maximemoreillon.com/articles?tag_id=111', category: 'Mechatronics'},
+  {image: require('@/assets/images/oshwlab.png'), label: 'OSHWLab', url: 'https://oshwlab.com/m.moreillon', category: 'Mechatronics'},
+  {image: require('@/assets/images/artifacthub.png'), label: 'Artifact Hub', url: 'https://artifacthub.io/packages/search?user=moreillon', category: 'Software development'},
+  {image: require('@/assets/images/thingiverse.svg'), label: 'Thingiverse', url: 'https://www.thingiverse.com/moreillon/designs', category: 'Mechatronics'},
+  {icon: ['fab','instagram'], label: 'Instagram', url: 'https://www.instagram.com/maxime_moreillon_modeling', category: 'Modeling'},
+  {image: require('@/assets/images/azs.jpg'), label: 'Agency', url: 'https://azs-group.jp/manage_post/990', category: 'Modeling'},
+  {image: require('@/assets/images/logo.png'), label: 'Profile', url: 'https://modeling.maximemoreillon.com', category: 'Modeling'},
+]
